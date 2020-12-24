dc772cc8219b8c4f362804b4c3caa26c2a6e8ffc
Espen Holtebu <esepn.holtebu@gmail.com>
2020-12-24 23:57:27 +0100

    Added register and login forms

--- brettspill_py/urls.py
+++ brettspill_py/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from brettspill import views
+from register.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('register/', register, name='register'),
+    path('', include('django.contrib.auth.urls')),
     path('',include('brettspill.urls'))
 ]
