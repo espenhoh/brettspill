5faaf77944f3595ab659d2128db7f731e0d1c059
Espen Holtebu <espen.holtebu@nav.no>
2021-12-31 12:27:03 +0100

    Breaking changes react

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import HomePage from "./HomePage";
+import HomePage from "./HomePage.js";
 
 export default class App extends Component {
   constructor(props) {
