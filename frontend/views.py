67467c629c87ba5c62c5672d133711a462c7b062
Espen Holtebu <espen.holtebu@nav.no>
2021-12-31 12:27:53 +0100

    Tester linter

--- frontend/views.py
+++ frontend/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request, *args, **kwargs):
     return render(request, 'frontend/index.html')
+
 
 def test(request, *args, **kwargs):
     return render(request, 'frontend/test.html')
