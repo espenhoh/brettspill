90992bac7d44dd8b59e5fb68eec52f1787466a4e
Espen Holtebu <espen.holtebu@nav.no>
2022-07-27 14:32:45 +0200

    Reset migrations

--- app/api/models.py
+++ app/api/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 # Create your models here.
+"""
 class Todo(models.Model):
     title = models.CharField(max_length=70)
 
     def __str__(self) -> str:
         return Todo.title
+"""
