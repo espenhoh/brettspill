fc087872418bdf97db87b333ece3a604abe91bf8
Espen Holtebu <espen.holtebu@nav.no>
2022-07-28 00:51:11 +0200

    Tar med Todo likevel

--- app/api/models.py
+++ app/api/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 
 # Create your models here.
-"""
+
 class Todo(models.Model):
     title = models.CharField(max_length=70)
 
     def __str__(self) -> str:
         return Todo.title
-"""
