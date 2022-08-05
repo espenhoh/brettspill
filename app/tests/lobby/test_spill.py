f515d1c9cbb591fc25dd3a312c357650b2afccf1
Espen Holtebu <espen.holtebu@nav.no>
2022-08-06 00:28:22 +0200

    Legger til datetime

--- app/tests/lobby/test_spill.py
+++ app/tests/lobby/test_spill.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
 from lobby.models import Spill
+from datetime import datetime
 
 
 class AnimalTestCase(TestCase):
     def setUp(self):
-        Spill.objects.create(spill_navn="test_spill", spill_type=Spill.SpillType.MONOPOL)
+        Spill.objects.create(spill_navn="test_spill", spill_type=Spill.SpillType.MONOPOL, start_tid=datetime.now(), slutt_tid=datetime.now())
 
     def test_spill_er_monopol(self):
         test_spill = Spill.objects.get(spill_navn="test_spill")
