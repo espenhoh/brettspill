d9f5df46e361141de1cc44374edd5ce837d10335
Espen Holtebu <espen.holtebu@nav.no>
2022-08-05 22:22:34 +0200

    Sender velkomst mail

--- app/brettspill_py/settings.py
+++ app/brettspill_py/settings.py
@@ -144,3 +144,12 @@ STATICFILES_DIRS = [
 
 LOGOUT_REDIRECT_URL = '/lobby/login/'
 LOGIN_REDIRECT_URL = '/'
+
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
--- app/lobby/views.py
+++ app/lobby/views.py
@@ -1,10 +1,14 @@
 # views.py
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib import messages
 from django.shortcuts import render, redirect, reverse
 from .forms import SpillerRegistreringForm
 # from django.contrib.auth.views import LoginView, LogoutView
 from django.views import View
+
+# To send emails
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 # Create your views here.
@@ -21,6 +25,15 @@ class RegisterView(View):
         form = SpillerRegistreringForm(request.POST)
         if form.is_valid():
             form.save()
+            self._send_velkomstepost(form)
             return redirect(reverse("login"))
         else:
             return render(request, "lobby/register.html", {"form": form})
+
+    def _send_velkomstepost(form):
+        subject = 'Brettspillregistrering'
+        message = 'Hei ! Takk for at du registrerte deg hos holtebu.eu brettspill.'
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = ['espenhoh@gmail.com', ]
+        send_mail(subject, message, email_from, recipient_list)
+        # return redirect('redirect to a new page')
