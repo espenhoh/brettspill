bcfdc56ddb1a09e339cd84cb6d19c1594a643d4e
Espen Holtebu <espen.holtebu@nav.no>
2022-08-06 00:28:02 +0200

    Sjekker om miljøvariable er satt korrekt

--- app/tests/test_settings.py
+++ app/tests/test_settings.py
@@ -3,7 +3,4 @@ import os
 
 def test_env():
     osdict = dict(os.environ)
-    print("User's Environment variable:")
-    print('Debug: ' + osdict['DEBUG'])
-    print('Postgres user : ' + osdict['POSTGRES_USER'])
-    assert True
+    assert int(osdict['DEBUG']) == 1
