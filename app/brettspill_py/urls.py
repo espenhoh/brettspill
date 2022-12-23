5bba55e3730d144142b93a20bc7f2e18a30066d6
Espen Holtebu <espen.holtebu@nav.no>
2022-12-23 23:50:22 +0100

    LEgger til router

--- app/brettspill_py/urls.py
+++ app/brettspill_py/urls.py
@@ -14,20 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from . import settings
+from lobby import views
+
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import RedirectView
+# from django.views.generic.base import RedirectView
+from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
+router = routers.DefaultRouter()
+router.register(r'users', views.SpillerViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     # path(r'api/auth/', include('knox.urls')), 
     path('admin/', admin.site.urls),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('lobby/', include('lobby.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # path('lobby/', include('lobby.urls')),
     # path('', include('gomoku.urls')),
     # path('api/', include('api.urls')),
     # path('frontend/', include('frontend.urls')),
