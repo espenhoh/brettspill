64a11f5217c90ac0b203697028c3e97790f96f4e
Espen Holtebu <espen.holtebu@nav.no>
2023-02-23 00:31:57 +0100

    Ny test

--- app/tests/lobby/test_views.py
+++ app/tests/lobby/test_views.py
@@ -1,50 +1,24 @@
 import pytest
+import json
 
 from django.test import TestCase
 
+from lobby.models import Spill
+from lobby.views import SpillViewSet
 
-def test_call_view_allow_anonymous(db, client):
-    response = client.get('/lobby/login', follow=True)
+from django.urls import reverse
+from rest_framework.test import APIRequestFactory
+
+factory = APIRequestFactory()
+
+
+def test_get_alle_spill_typer():
+    viewset = SpillViewSet()
+    request = factory.get(reverse('spill-get-alle-spill-typer'))
+    response = viewset.get_alle_spill_typer(request)
     assert response.status_code == 200
-    response = client.post('/lobby/login', follow=True)
-    assert response.status_code == 200
-
-
-def test_call_view_load(db, client):
-    client.login(username='admin', password='admin')  # defined in fixture or with factory in setUp()
-    response = client.get('/lobby/login/')
-    assert response.status_code == 200
-    TestCase().assertRedirects(response, '/lobby/')
-
-
-"""
-class TestCalls(TestCase):
-    def test_call_view_allow_anonymous(self):
-        response = self.client.get('/lobby/login', follow=True)
-        self.assertEqual(response.status_code, 200)
-        response = self.client.post('/lobby/login', follow=True)
-        self.assertEqual(response.status_code, 200)
-
-    def test_call_view_load(self):
-        self.client.login(username='admin', password='admin')  # defined in fixture or with factory in setUp()
-        response = self.client.get('/lobby/login')
-        self.assertEqual(response.status_code, 200)
-        self.assertRedirects(response, '/lobby')
-
-    @pytest.mark.skip(reason="todo")
-    def test_call_view_fail_blank(self):
-        self.client.login(username='user', password='test')
-        response = self.client.post('/url/to/view', {}) # blank data dictionary
-        self.assertFormError(response, 'form', 'some_field', 'This field is required.')
-        # etc. ...
-
-    @pytest.mark.skip(reason="todo")
-    def test_call_view_fail_invalid(self):
-        # as above, but with invalid rather than blank data in dictionary
-        pass
-
-    @pytest.mark.skip(reason="todo")
-    def test_call_view_success_invalid(self):
-        # same again, but with valid data, then
-        self.assertRedirects(response, '/contact/1/calls/')
-        """
+    expected_spill_typer = {'MPOL': 'Monopol'}
+    response_data = json.loads(response.data)
+    for key, value in expected_spill_typer.items():
+        assert key in response_data
+        assert response_data[key] == value
