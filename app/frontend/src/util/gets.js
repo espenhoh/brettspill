c074ed55f5303a2b83ccbaedc0587830c2dd7f65
Espen Holtebu <espen.holtebu@nav.no>
2023-03-19 21:53:08 +0100

    Forenkler

--- app/frontend/src/util/gets.js
+++ app/frontend/src/util/gets.js
@@ -14,31 +14,25 @@ export async function getSpill(id) {
   return data;
 }
 
-async function get(url) {
-  try {
-    const response = await axios.get(url);
-
-    if (!erOK(response)) {
-      throw { message: "Failed to fetch spill.", status: response.status };
-    }
-
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 export async function getSpillTyper() {
-  try {
-    const response = await axios.get(
-      "/lobby/spill/get_alle_spill_typer/",
-      {
-        headers: { Accept: "application/json" },
-      }
-    );
-
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+    return await get("/lobby/spill/get_alle_spill_typer/");
 };
+
+async function get(url) {
+    try {
+      const response = await axios.get(
+        url,
+        {
+            headers: { Accept: "application/json" },
+          }
+        );
+  
+      if (!erOK(response)) {
+        throw { message: "Failed to fetch.", status: response.status };
+      }
+  
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
