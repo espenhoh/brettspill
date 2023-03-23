da7c2d9ba5483e00502fadb5dcdee5cd0804d4f7
Espen Holtebu <espen.holtebu@nav.no>
2023-03-23 22:43:50 +0100

    format

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -7,7 +7,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import Login, {loginAction} from "./pages/Login";
+import Login, { loginAction } from "./pages/Login";
 import Spilliste from "./pages/Spilliste";
 import SpillLobby from "./pages/SpillLobby";
 import CreateGame, { spillTypeLoader } from "./pages/CreateGame";
@@ -21,11 +21,20 @@ import { getSpillListe, getSpill, getSpillTyper } from "./util/gets";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />} errorElement={<Feilside/>}>
+    <Route path="/hjem" element={<Layout />} errorElement={<Feilside />}>
       <Route index element={<Navigate replace to="login" />} />
-      <Route path="spill/" element={<Spilliste />} loader={getSpillListe} action={loginAction}/>
-      <Route path="spill/:spillId" element={<SpillLobby />} loader={({params}) => getSpill(params.spillId)} />
-      <Route path="lag_spill" element={<CreateGame />} loader={getSpillTyper}/>
+      <Route
+        path="spill/"
+        element={<Spilliste />}
+        loader={getSpillListe}
+        action={loginAction}
+      />
+      <Route
+        path="spill/:spillId"
+        element={<SpillLobby />}
+        loader={({ params }) => getSpill(params.spillId)}
+      />
+      <Route path="lag_spill" element={<CreateGame />} loader={getSpillTyper} />
       <Route path="login" element={<Login />} />
       <Route path="register" element={<Register />} />
       <Route path="*" element={<IkkeFunnet />} />
