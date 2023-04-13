21df7779c47ab5322d36388f3eed2d28ebaacd6b
Espen Holtebu <espen.holtebu@nav.no>
2023-04-13 23:03:11 +0200

    Legger til sikker på å slette

--- app/frontend/src/pages/Spilliste.js
+++ app/frontend/src/pages/Spilliste.js
@@ -2,22 +2,23 @@ import React, { useEffect, useState, useCallback } from "react";
 import { Link, useLoaderData, useNavigate } from "react-router-dom";
 import dayjs from "dayjs";
 
-import { slettSpill } from '../util/deletes';
+import { slettSpill } from "../util/deletes";
 
 //import styles from "./Home.module.css";
 
 const DATE_FORMAT = "DD/MM/YY h:m";
 
-const liste = [{
-  "id": 9999,
-  "spill_type_navn": "Fire på rad",
-  "spill_navn": "Dummyspill",
-  "spill_type": "4PRA",
-  "opprettet_tid": "2023-02-02T21:34:33.692353Z",
-  "start_tid": null,
-  "slutt_tid": null,
-  "spillere": []
-}
+const liste = [
+  {
+    id: 9999,
+    spill_type_navn: "Fire på rad",
+    spill_navn: "Dummyspill",
+    spill_type: "4PRA",
+    opprettet_tid: "2023-02-02T21:34:33.692353Z",
+    start_tid: null,
+    slutt_tid: null,
+    spillere: [],
+  },
 ];
 
 const Spilliste = (props) => {
@@ -28,12 +29,16 @@ const Spilliste = (props) => {
     document.title = "Spilliste";
   }, []);
 
-
   const slettSpillHandler = (spillId) => {
     return async () => {
-      console.log(`Sletter ${spillId}`);
-      await slettSpill(spillId);
-      navigate('/spill/', { replace: true });
+      const sikker_paa_aa_slette = window.confirm(
+        "Sikker på at du vil slette spillet??"
+      );
+      if (sikker_paa_aa_slette) {
+        console.log(`Sletter ${spillId}`);
+        await slettSpill(spillId);
+        navigate("/spill/", { replace: true });
+      }
     };
   };
 
@@ -56,11 +61,21 @@ const Spilliste = (props) => {
             <tr key={spill.id}>
               <td>
                 <Link to={`${spill.id}/`}>{spill.spill_navn}</Link>
-              </td> 
+              </td>
               <td>{spill.spill_type_navn}</td>
-              <td>{spill.start_tid ? dayjs(spill.start_tid).format(DATE_FORMAT) : 'Ikke startet'}</td>
-              <td>{spill.slutt_tid ? dayjs(spill.slutt_tid).format(DATE_FORMAT) : 'Ikke ferdig'}</td>
-              <td><button onClick={slettSpillHandler(spill.id)}>slett</button></td>
+              <td>
+                {spill.start_tid
+                  ? dayjs(spill.start_tid).format(DATE_FORMAT)
+                  : "Ikke startet"}
+              </td>
+              <td>
+                {spill.slutt_tid
+                  ? dayjs(spill.slutt_tid).format(DATE_FORMAT)
+                  : "Ikke ferdig"}
+              </td>
+              <td>
+                <button onClick={slettSpillHandler(spill.id)}>slett</button>
+              </td>
             </tr>
           ))}
         </tbody>
